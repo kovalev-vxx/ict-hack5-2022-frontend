2727eb7557fbe1e0e523b4be9aa4c383840c1d90
Kovalev Valery <kovalev.vxx@gmail.com>
2022-12-10 15:21:30 +0300

    taildwind installed

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,15 +6,16 @@ const MainPage = () => {
     return (
         <BasePage>
             <Row justify="space-around" align="middle">
-                <Col style={{display:"flex", textAlign:"center", flexDirection:"column", justifyContent:"space-between"}}>
+                {/*style={{display:"flex", textAlign:"center", flexDirection:"column", justifyContent:"space-between"}}*/}
+                <Col className="flex-col text-center justify-center" >
                     <div style={{maxWidth:"576px"}}>
                         <h1 className="mainTitle">ЦИФРОВАЯ ПЛАТФОРМА</h1>
                         <h2>ИТМО X ПИШ</h2>
                     </div>
-                        <Row style={{padding:"0 70px"}}>
-                            <Col span={12}><Button>Я студент</Button></Col>
-                            <Col span={12}><Button>Мы компания</Button></Col>
-                        </Row>
+                    <div className="flex justify-center gap-5">
+                        <Button>Я студент</Button>
+                        <Button>Мы компания</Button>
+                    </div>
                 </Col>
             </Row>
         </BasePage>
